db3b6a5fa061b53d97d8aa4b512a233d94243ee4
deaconwu <71239492+deaconwu@users.noreply.github.com>
2020-09-28 13:28:42 +0800

    Add files via upload

---
+++ Datagram/CHistoryRecord.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <windows.h>
+#include "InfoType.h"
+
+class CHistoryRecord
+{
+public:
+	CHistoryRecord();
+	~CHistoryRecord()
+	{
+		if (NULL != m_pInstance)
+		{
+			delete m_pInstance;
+			m_pInstance = NULL;
+		}
+	}
+
+	static CHistoryRecord* GetInstance()
+	{
+		if (NULL == m_pInstance)
+			m_pInstance = new CHistoryRecord;
+
+		return m_pInstance;
+	}
+
+	void OnRecord(WORD wDayOfWeek);
+
+	void YesterdayAppend(long lVehicleNum);
+
+	void OnMove();
+
+private:
+
+	long FindVinPos(uint8_t pVin[]);
+
+	long InsertVinAndSort(uint8_t pVin[]);
+
+	HANDLE m_hThread;
+
+	static CHistoryRecord* m_pInstance;
+
+	//上周数据
+	uint8_t m_chVinLastWeek[MAX_VEHICLENUM][VIN_LENGTH + 1]; //每辆车vin码
+	long m_vehicleNumLastWeek;
+
+	//本周数据
+	uint8_t m_chVinThisWeek[MAX_VEHICLENUM][VIN_LENGTH + 1];
+	long m_vehicleNumThisWeek;
+
+	//最近数据(今日以外)
+};
